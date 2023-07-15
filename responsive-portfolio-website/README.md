22f537a09e65f45a0011479c71880f9190b8fe9e
Julia Koverzneva <106422712+yukoverzneva@users.noreply.github.com>
2023-07-15 14:33:27 +0400

    add files

---
+++ responsive-portfolio-website/README.md
@@ -0,0 +1,10 @@
+# 💼 Responsive Personal Portfolio Website
+## Made with help of video (https://youtu.be/5-_2z-DdWng)
+### 💼 Responsive Personal Portfolio Website
+
+- Responsive Personal Portfolio Website Design Using HTML CSS & JavaScript
+- Includes a light and dark theme.
+- Contains animations when scrolling.
+- Includes a form to send emails.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
---
+++ responsive-portfolio-website/assets/js/main.js
@@ -0,0 +1,169 @@
+/*=============== SHOW MENU ===============*/
+const navMenu=document.getElementById('nav-menu'),
+navToggle=document.getElementById('nav-toggle'),
+navClose=document.getElementById('nav-close')
+
+if(navToggle){
+  navToggle.addEventListener('click',()=>{
+    navMenu.classList.add('show-menu')
+  })
+}
+
+if (navClose){
+  navClose.addEventListener('click',()=>{
+    navMenu.classList.remove('show-menu')
+  })
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink=document.querySelectorAll('.nav__link')
+const linkAction=()=>{
+const navMenu=document.getElementById('nav-menu')
+navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=>n.addEventListener('click',linkAction))
+/*=============== SWIPER PROJECTS ===============*/
+let swiperProjects = new Swiper('.projects__container', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+  spaceBetween: 24,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints:{
+    1200:{
+      slidesPerView: 2,
+      spaceBetween: -56,
+    },
+  },
+});
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+contactName = document.getElementById('contact-name'),
+contactEmail = document.getElementById('contact-email'),
+contactProject = document.getElementById('contact-project'),
+contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e)=>{
+e.preventDefault()
+
+// Check if the field has a value
+if(contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+  //Add and remove color
+  contactMessage.classList.remove('color-blue')
+  contactMessage.classList.add('color-red')
+  //Show message
+  contactMessage.textContent = 'Fill in all the fields 📩'
+} else {
+  //serviceID - templateID - #form - publicKey
+  emailjs.sendForm('service_wkvqznr','template_4fnvl4q','#contact-form','9iOgcuZIWf6EWtjWR').then(()=>{
+    //Show message and add color
+    contactMessage.classList.add('color-blue')
+    contactMessage.textContent='Message sent ✅'
+
+    //Remove message after 5s
+    setTimeout(() => {
+      contactMessage.textContent=''
+    }, 5000);
+  },(error)=>{
+    alert('Something went wrong!', error)
+
+  })
+
+  //Clear the input field
+  contactName.value=''
+  contactEmail.value=''
+  contactProject.value=''
+}
+}
+contactForm.addEventListener('submit', sendEmail)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections=document.querySelectorAll('section[id]')
+
+const scrollActive=()=>{
+  const scrollY=window.pageYOffset
+
+  sections.forEach(current=>{
+    const sectionHeight=current.offsetHeight,
+          sectionTop=current.offsetTop-58,
+          sectionId=current.getAttribute('id'),
+          sectionsClass=document.querySelector('.nav__menu a[href*=' + sectionId +']')
+
+    if(scrollY>sectionTop && scrollY<= sectionTop +sectionHeight){
+        sectionsClass.classList.add('active-link')
+    } else {
+      sectionsClass.classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp=()=>{
+  const scrollUp=document.getElementById('scroll-up')
+  //When the scroll is hugher than 350wh, add the show-scroll class to the a tag with the scrollup
+  this.scrollY>=350?scrollUp.classList.add('show-scroll'):scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
+/*=============== DARK LIGHT THEME ===============*/ 
+const  themeButton=document.getElementById('theme-button')
+const darkTheme='dark-theme'
+const iconTheme='ri-sun-line'
+//Previously selected topic (if user selected)
+const selectedTheme=localStorage.getItem('selected-theme')
+const selectedIcon=localStorage.getItem('seleted-icon')
+
+//We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme=()=>document.body.classList.contains(darkTheme)?'dark':'light'
+const getCurrentIcon=()=>document.classList.contains(iconTheme)?'ri-moon-line':'ri-sun-line'
+
+//We validate if the user previously chose a topic
+if (selectedTheme){
+  //If the validation is fullfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme==='dark'?'add':'remove'](darkTheme)
+  themeButton.classList[selectedIcon==='ri-moon-line'?'add':'remove'](iconTheme)
+}
+//Activate/deactivate the theme manually with the button
+themeButton.addEventListener('click',()=>{
+  //Add or remove the dark/icon theme
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  //We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme',getCurrentTheme())
+  localStorage.setItem('selected-icon',getCurrentIcon())
+  
+})
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader=()=>{
+  const header=document.getElementById('header')
+  //When the scroll is greater than 50 wh, add the scroll-header to the header tag
+  this.scrollY>=50?header.classList.add('bg-header'): header.classList.remove('bg-header')
+}
+window.addEventListener('scroll',scrollHeader)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr=ScrollReveal({
+  origin:'top',
+  distance:'60px',
+  duration:1500,
+  delay:200,
+  // reset:true //Animations repeat
+})
+sr.reveal(`.home__container,.projects__container, .footer__container`)
+// sr.reveal(`.home__info div`, {delay:600, origin:'bottom',interval:100})
+// sr.reveal(`.skills__content:nth-child(1)`, {origin:left})
+// sr.reveal(`.skills__content:nth-child(2)`, {origin:right})
+sr.reveal(`.qualification__content,.services__card`, {interval:100})
